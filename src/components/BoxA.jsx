9ba191f2e81529abe7d86ad70c31979f6a64ccb9
LoganjwSim <j1sim234@gmail.com>
2023-11-07 17:19:14 +0900

    first commit

---
+++ src/components/BoxA.jsx
@@ -0,0 +1,9 @@
+const BoxA = () => {
+  return (
+    <div className="bg-red-500 flex justify-center items-center w-32 h-32">
+      BoxA
+    </div>
+  );
+};
+
+export default BoxA;
---
+++ src/components/BoxB.jsx
@@ -0,0 +1,9 @@
+const BoxB = () => {
+  return (
+    <div className="bg-blue-500 text-white flex justify-center items-center w-32 h-32">
+      BoxB
+    </div>
+  );
+};
+
+export default BoxB;
